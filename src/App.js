5733f403aecaab286e46fa80946328b314434fbd
deen john <john@deens-MacBook-Pro.local>
2024-09-18 00:09:16 -0700

    hit nginx api endpoint rather than node server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   async function getData() {
-    const url = "http://127.0.0.1:4000/";
+    const url = "/api";
     try {
       const response = await fetch(url);
       if (!response.ok) {
