f1dd237efab6039f33ad59968f00b8e5b3d7fd24
deen john <john@deens-MacBook-Pro.local>
2024-09-18 00:48:09 -0700

    fix server.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  async function getData() {
-    const url = "/api/users";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const url = "/api/users";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        setUsers(JSON.parse(response.json()))
+      } catch (error) {
+        console.error(error.message);
       }
-
-    } catch (error) {
-      console.error(error.message);
     }
-  }
-  getData()
+    getData()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {users.map((user) => {
+          <p>{user.name}</p>
+        })}
       </header>
     </div>
   );
